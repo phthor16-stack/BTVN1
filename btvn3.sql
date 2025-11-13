800db85c915c8fb006ea6a5ec0b39ca70cd7d031
thao <phthor16@gmail.com>
2025-11-14 00:36:46 +0700

    btvn3

---
+++ btvn3.sql
@@ -0,0 +1,5 @@
+INSERT INTO Employees (FirstName, LastName, HireDate, Salary)
+VALUES
+('Nguyen', 'An', '2023-05-10', 1200),
+('Tran', 'Binh', '2022-11-20', 1500),
+('Le', 'Hoa', '2024-01-02', 1800);
